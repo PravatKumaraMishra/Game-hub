9d81bed963e0ac78eb52e37e209da5ff88c77d6e
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2025-11-21 21:42:47 +0530

    add a resposive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 export default function App() {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dogerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
