1bb5af3da3af9b3360657952462572ebab7a1a3a
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2025-11-24 23:44:17 +0530

    Implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
