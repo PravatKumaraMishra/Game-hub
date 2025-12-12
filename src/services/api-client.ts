f0c2505da70ff051d370fefe4faf963caffb6873
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2025-12-12 13:44:18 +0530

    Hide api key in env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f6750362b20645f4994825acd3e7031a",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
 
